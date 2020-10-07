ddb4335b89aed02b948231ea475db972e11737c6
Jafer Haider <itsjafer@gmail.com>
2020-10-07 04:07:22 +0000

    Updated lambda function with fully functional parsing

--- queryParser/queryParser/lambda_function.py
+++ queryParser/queryParser/lambda_function.py
@@ -4,13 +4,26 @@ from base64 import b64decode
 
 def lambda_handler(event, context):
     # TODO implement
-    body = event['body']
-    print(body)
+    body = json.loads(event['body'])
+    print(body['content'])
     
-    resume = body['resume']
-    bytes = b64decode(resume, validate=True)
+    resume = b64decode(body['content'])
+    print(resume)
+    print(type(resume))
     
     # resume = request.files['file']
-    response = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=bytes))
+    parseResponse = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=resume))
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Methods": "POST"
+        },
+        "body": json.dumps(parseResponse.json())
+    };
+    
+    print(response)
 
-    return response.json()
+    return response
