9bdbaf3201ccdde2127a722cf0fabdb19838c043
Jafer Haider <itsjafer@gmail.com>
2020-10-12 20:28:01 +0000

    Removed debug statements and added comments

--- queryParser/queryParser/lambda_function.py
+++ queryParser/queryParser/lambda_function.py
@@ -3,27 +3,24 @@ import json
 from base64 import b64decode
 
 def lambda_handler(event, context):
-    # TODO implement
+    # get the request body from API gateway
     body = json.loads(event['body'])
-    print(body['content'])
     
+    # decode the resume from base64
     resume = b64decode(body['content'])
-    print(resume)
-    print(type(resume))
     
-    # resume = request.files['file']
+    # ask Lever to parse our resume
     parseResponse = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=resume))
+    
+    # return a response for API gateway
     response = {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://itsjafer.com",
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Methods": "POST"
         },
         "body": json.dumps(parseResponse.json())
     };
-    
-    print(response)
-
     return response
