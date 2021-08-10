8d02357db77c8e2ae88272ec64909bb4bfe9bd7b
Jafer Haider <itsjafer@gmail.com>
2021-08-10 18:11:10 +0000

    Update cache time

--- main.py
+++ main.py
@@ -17,9 +17,8 @@ def handler(request):
         # header and caches preflight response for an 3600s
         headers = {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
         }
 
         return ('', 204, headers)
